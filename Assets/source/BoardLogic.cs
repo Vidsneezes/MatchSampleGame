04a8e640c4382bd142bd55015c201f78e8fc70e9
Vidsneezes <moraleszez@gmail.com>
2017-03-27 15:18:34 -0600

    board logic- added board matrix and fill method

---
+++ Assets/source/BoardLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardLogic {
+
+    private int boardWidth;
+    private int boardHeight;
+
+    private int[] boardMatrix;
+
+    public BoardLogic()
+    {
+        boardMatrix = new int[boardWidth * boardHeight];
+        InitBoard();
+    }
+
+    public void InitBoard()
+    {
+        for (int i = 0; i < boardWidth*boardHeight; i++)
+        {
+            boardMatrix[i] = -1;
+        }
+    }
+
+
+}
