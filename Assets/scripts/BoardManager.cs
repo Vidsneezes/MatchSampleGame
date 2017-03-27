5f0189725e54ce9d0f7564607147063494da0264
Vidsneezes <moraleszez@gmail.com>
2017-03-27 15:13:39 -0600

    added a board manager script

---
+++ Assets/scripts/BoardManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager {
+
+    public int boardWidth;
+    public int boardHeight;
+
+    public BoardManager()
+    {
+
+    }
+
+}
