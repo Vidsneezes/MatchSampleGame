5a8a5dead53de32e4a119356747998a03f860da6
Vidsneezes <moraleszez@gmail.com>
2017-03-28 12:01:22 -0600

    added connection counter

--- Assets/source/BoardLogic.cs
+++ Assets/source/BoardLogic.cs
@@ -27,7 +27,7 @@ public class BoardLogic {
 
     public void InitBoard()
     {
-        for (int i = 0; i < boardWidth*boardHeight; i++)
+        for (int i = 0; i < boardWidth * boardHeight; i++)
         {
             boardMatrix[i] = -1;
         }
@@ -42,7 +42,7 @@ public class BoardLogic {
                 int pieceIndex = i + j * boardWidth;
                 boardMatrix[pieceIndex] = Mathf.FloorToInt(Random.Range(0, 1f) * 5);
             }
-        }   
+        }
     }
 
     public void ClearConnectedType(int type, int x, int y)
@@ -52,7 +52,64 @@ public class BoardLogic {
 
     public bool CanMove(int orX, int orY, int newX, int newY)
     {
+        int newIndex = newX + newY * boardWidth;
+        int original = boardMatrix[orX + orY * boardWidth];
+        int positionalIndex = newX + newY * boardWidth;
+        int possibleConnections = 0;
+
+        //Check Up of new
+        if (newY - 1 >= 0 && boardMatrix[newX + (newY - 1) * boardWidth] == original)
+        {
+            possibleConnections += 1;
+        }
+        //CHeck down of new
+        if (newY + 1 < boardHeight && boardMatrix[newX + (newY + 1) * boardWidth] == original)
+        {
+            possibleConnections += 1;
+        }
+        //check left of new
+        if (newX - 1 >= 0 && boardMatrix[(newX - 1) + newY * boardWidth] == original)
+        {
+            possibleConnections += 1;
+        }
+        //check right of new
+        if (newX + 1 < boardWidth && boardMatrix[(newX + 1) + newY * boardWidth] == original)
+        {
+            possibleConnections += 1;
+        }
+        if (possibleConnections >= 2)
+        {
+            return true;
+        }
         return false;
+    }
+
+    public int ConnectionCounter(int original,int newX, int newY)
+    {
+        int possibleConnections = 0;
+
+        //Check Up of new
+        if (newY - 1 >= 0 && boardMatrix[newX + (newY - 1) * boardWidth] == original)
+        {
+            possibleConnections += 1;
+        }
+        //CHeck down of new
+        if (newY + 1 < boardHeight && boardMatrix[newX + (newY + 1) * boardWidth] == original)
+        {
+            possibleConnections += 1;
+        }
+        //check left of new
+        if (newX - 1 >= 0 && boardMatrix[(newX - 1) + newY * boardWidth] == original)
+        {
+            possibleConnections += 1;
+        }
+        //check right of new
+        if (newX + 1 < boardWidth && boardMatrix[(newX + 1) + newY * boardWidth] == original)
+        {
+            possibleConnections += 1;
+        }
+
+        return possibleConnections;
     }
 
     public int[] GetBoard()
