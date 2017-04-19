fc31c37438e6c7c82c33a15aa2f4ce73ce559238
Vidsneezes <moraleszez@gmail.com>
2017-04-18 19:18:51 -0600

    added highscore scene container

---
+++ Assets/scripts/HighScoreSceneContainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreSceneContainer : MonoBehaviour {
+
+
+    public Button startButton;
+    public Text highscoreText;
+    public Text localScoreText;
+
+    public Action onContinueButtonClicked;
+
+    // Use this for initialization
+    private void Start()
+    {
+        startButton.onClick.AddListener(OnContinueButtonClicked);
+        highscoreText.text = "highscore : 00000000";
+        localScoreText.text = "00000000";
+    }
+
+    private void OnContinueButtonClicked()
+    {
+        if (onContinueButtonClicked != null)
+        {
+            onContinueButtonClicked();
+        }
+    }
+}
