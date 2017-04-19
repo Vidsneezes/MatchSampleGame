16bda84c5c3d6c3ec2e5e7401c93cfc5a234b10b
Vidsneezes <moraleszez@gmail.com>
2017-04-18 19:03:54 -0600

    added mainmenucontainer

---
+++ Assets/scripts/MainMenuContainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuContainer : MonoBehaviour {
+
+    public Button startButton;
+    public Text highscoreText;
+
+    public Action onStartButtonClicked;
+
+	// Use this for initialization
+	private void Start () {
+        startButton.onClick.AddListener(OnStartButtonClicked);
+        highscoreText.text = "highscore : 00000000";
+
+	}
+	
+	private void OnStartButtonClicked()
+    {
+        if(onStartButtonClicked != null)
+        {
+            onStartButtonClicked();
+        }
+    }
+}
